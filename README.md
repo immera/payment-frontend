ff0d923b3eed8d28a35c5f805f0013fd1b6dbf63
Harikrushna <adiechahari@gmail.com>
2022-05-15 16:27:34 +0530

    added package details

--- README.md
+++ README.md
@@ -2,3 +2,40 @@
 
 This package is specifically to used with laravel backed package in order to handler frontend complecations using this package only and actual consumer of this package will only need to call method respectively with proper configurations.
 
+## Configuration
+
+## Dependancies
+
+### stripe
+make sure your stripe script `<script src="https://js.stripe.com/v3/"></script>` (ref: https://stripe.com/docs/js/including)
+
+* Incase of nuxt - you can add it in the nuxt config like this
+
+```js
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    ...
+    script: [
+      { src: "https://js.stripe.com/v3/" }
+    ]
+  },
+  ...
+}
+```
+
+This dependancy we could not extract out from the application and keep the to scoped in the package level, so we have to keep it this way.
+
+Honestly I'm not very happy with this type of dependacy but I don't get any better way.
+
+
+### axios
+for dependancy on axios we allow to pass the instance of axios while setting up payment object.
+i.e.
+```js
+new Payment(settings, axios)
+```
+
+here the second parameter is axios dependancy, 
+
+We implemented it this way to keep your existing settings as it is and you just need to pass the dependacy to use it for your server calls.
---
+++ src/index.js
@@ -0,0 +1,94 @@
+
+const Payment = function(settings, axios) {
+    this._settings = settings;
+    this._axios = axios;
+    this._stripe = Stripe(settings.key)
+    this.__proto__ = {
+      _requestPayment: function(payment_method, options) {
+        return this._axios.post('/payment/request', {
+          "return_url": this._settings.callback,
+          "payment_method": payment_method,
+          ...options
+        })
+      },
+      alipay: function(options) {
+        this._requestPayment('alipay', options)
+          .then(({data}) => {
+            this._stripe.confirmAlipayPayment(
+              data.response.client_secret, {
+                return_url: data.callback
+              }
+            ).then(function(result) {
+              if (result.error) {
+                // Inform the customer that there was an error.
+                console.error(error);
+              }
+            }).catch(console.error);
+          })
+      },
+      wechat_pay: function(options) {
+        this._requestPayment('wechat_pay', options)
+          .then(({data}) => {
+            this._stripe.confirmWechatPayPayment(
+              data.response.client_secret,
+              {
+                payment_method_options: {
+                  wechat_pay: { client: 'web' }
+                }
+              }
+            ).then(({error, paymentIntent}) => {
+              if (error) {
+                // Inform the customer that there was an error.
+                console.error(error);
+              } else {
+                console.log(data, paymentIntent)
+                // this._axios.post(data.callback, paymentIntent)
+                //   .then(console.log)
+                //   .catch(console.error)
+              }
+            });
+          })
+      },
+      initCard: function(cardId) {
+        let element = this._stripe.elements();
+        this.cardElem = element.create('card');
+        this.cardElem.mount(cardId)
+      },
+      card: function(options) {
+        this._requestPayment('card', options)
+          .then(({data}) => {
+            this._stripe.confirmCardPayment(
+              data.response.client_secret, {
+                payment_method: {card: this.cardElem}
+              }
+            ).then(function(result) {
+              if (result.error) {
+                // Inform the customer that there was an error.
+                console.error(error);
+              }
+              console.log("Possibly successful payment")
+              console.log(result)
+            }).catch(console.error);
+          })
+      },
+      multibanco: function(options) {
+        let {amount, currency} = options;
+        this._stripe.createSource({
+          type: 'multibanco',
+          amount, currency
+          owner: {
+            name: 'Jenny Rosen',
+            email: 'jenny.rosen@example.com',
+          },
+          redirect: {
+            return_url: `{this._settings.api}/payment/webhook`,
+          },
+        }).then(console.log);
+      },
+      pay: function({payment_method, ...options}) {
+        this[payment_method](options)
+      }
+    }
+  }
+  
+  module.exports = Payment
