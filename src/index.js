a22bf44ef6f731c264c8e747c9120e1dd0d9c589
Antonio Almeida <promatik@gmail.com>
2022-06-08 23:19:55 +0100

    Added ESLint Removed node_modules Added some comments

--- src/index.js
+++ src/index.js
@@ -1,155 +1,172 @@
-const { loadScript } = require("@paypal/paypal-js");
+const { loadScript } = require('@paypal/paypal-js');
 
-const Payment = function(settings, axios) {
-    this._settings = settings;
-    this._axios = axios;
-    this._stripe = Stripe(settings.key)
-    this.paypal = null
-    this.__proto__ = {
-      _requestPayment: function(payment_method, options) {
-        return this._axios.post('/payment/request', {
-          "return_url": this._settings.callback,
-          "payment_method": payment_method,
-          ...options
-        })
-      },
-      alipay: function(options) {
-        return this._requestPayment('alipay', options)
-          .then(({data}) => {
-            this._stripe.confirmAlipayPayment(
-              data.response.client_secret, {
-                return_url: data.callback
-              }
-            ).then(function(result) {
-              if (result.error) {
-                // Inform the customer that there was an error.
-                console.error(error);
-              }
-            }).catch(console.error);
-          })
-      },
-      wechat_pay: function(options) {
-        return this._requestPayment('wechat_pay', options)
-          .then(({data}) => {
-            this._stripe.confirmWechatPayPayment(
-              data.response.client_secret,
-              {
-                payment_method_options: {
-                  wechat_pay: { client: 'web' }
-                }
-              }
-            ).then(({error, paymentIntent}) => {
-              if (error) {
-                // Inform the customer that there was an error.
-                console.error(error);
-              } else {
-                console.log(data, paymentIntent)
-                // this._axios.post(data.callback, paymentIntent)
-                //   .then(console.log)
-                //   .catch(console.error)
-              }
-            });
-          })
-      },
-      initCard: function(cardId) {
-        let element = this._stripe.elements();
-        this.cardElem = element.create('card');
-        this.cardElem.mount(cardId)
-      },
-      initPaypalBtn: function(btnId, {currency, amount}, options = {}) {
-        currency = currency.toUpperCase()
-        amount = (number(amount)/100).toFixed(2);
+const Payment = (settings, axios) => {
+  this.settings = settings;
+  // TODO remove axios and use fetch instead
+  this.axios = axios;
+  // TODO import Stripe(?)
+  this.stripe = Stripe(settings.key);
+  this.paypal = null;
 
+  // TODO remove __proto__ and declare methods in a class
+  this.__proto__ = {
+    requestPayment(payment_method, options) {
+      return this.axios.post('/payment/request', {
+        return_url: this.settings.callback,
+        payment_method,
+        ...options,
+      });
+    },
+
+    alipay(options) {
+      return this.requestPayment('alipay', options).then(({ data }) => {
+        this.stripe
+          .confirmAlipayPayment(data.response.client_secret, {
+            return_url: data.callback,
+          })
+          .then(result => {
+            if (result.error) {
+              // Inform the customer that there was an error.
+              console.error(error);
+            }
+          })
+          .catch(console.error);
+      });
+    },
+
+    wechat_pay(options) {
+      return this.requestPayment('wechat_pay', options).then(({ data }) => {
+        this.stripe
+          .confirmWechatPayPayment(data.response.client_secret, {
+            payment_method_options: {
+              wechat_pay: { client: 'web' },
+            },
+          })
+          .then(({ error, paymentIntent }) => {
+            if (error) {
+              // Inform the customer that there was an error.
+              console.error(error);
+            } else {
+              console.log(data, paymentIntent);
+              // this.axios.post(data.callback, paymentIntent)
+              //   .then(console.log)
+              //   .catch(console.error)
+            }
+          });
+      });
+    },
+
+    initCard(cardId) {
+      const element = this.stripe.elements();
+      this.cardElem = element.create('card');
+      this.cardElem.mount(cardId);
+    },
+
+    async initPaypalBtn(btnId, { currency, amount }) {
+      currency = currency.toUpperCase();
+      amount = (number(amount) / 100).toFixed(2);
+
+      try {
+        this.paypal = await loadScript({
+          'client-id': 'test',
+          currency,
+        });
+      } catch (error) {
+        console.error('failed to load the PayPal JS SDK script', error);
+      }
+      if (paypal) {
         try {
-          this.paypal = await loadScript({ "client-id": "test" , "currency": currency });
-        } catch (error) {
-          console.error("failed to load the PayPal JS SDK script", error);
-        }
-        if (paypal) {
-          try {
-            await this.paypal.Buttons({
+          await this.paypal
+            .Buttons({
               style: {
                 layout: 'horizontal',
-                color: "blue",
+                color: 'blue',
                 tagline: false,
               },
-              createOrder: function(data, actions) {
+              createOrder(data, actions) {
                 // Set up the transaction
                 return actions.order.create({
-                  purchase_units: [{
-                    amount: {
-                      currency_code: currency,
-                      value: amount
-                    }
-                  }]
+                  purchase_units: [
+                    {
+                      amount: {
+                        currency_code: currency,
+                        value: amount,
+                      },
+                    },
+                  ],
                 });
               },
-              onApprove: function(data, actions) {
+              onApprove(data, actions) {
                 // This function captures the funds from the transaction.
-                return actions.order.capture().then(function(details) {
+                return actions.order.capture().then(details => {
                   // This function shows a transaction success message to your buyer.
-                  alert('Transaction completed by ' + details.payer.name.given_name);
+                  // TODO emit event
+                  console.log(
+                    `Transaction completed by ${details.payer.name.given_name}`
+                  );
                 });
               },
-              onCancel: function (data) {
+              onCancel(data) {
                 // Show a cancel page, or return to cart
-                console.log("Cancelled !!!", data)
+                console.log('Cancelled !!!', data);
               },
-              onError: function (err) {
+              onError(err) {
                 // For example, redirect to a specific error page
-                console.error(err)
+                console.error(err);
                 // window.location.href = "/your-error-page-here";
-              }
-            }).render(btnId);
-          } catch (error) {
-            console.error("failed to render the PayPal Buttons", error);
-          }
+              },
+            })
+            .render(btnId);
+        } catch (error) {
+          console.error('failed to render the PayPal Buttons', error);
+        }
       }
-  
+    },
 
-
-
-
-      },
-      card: function(options) {
-        return this._requestPayment('card', options)
-          .then(({data}) => {
-            this._stripe.confirmCardPayment(
-              data.response.client_secret, {
-                payment_method: {card: this.cardElem}
-              }
-            ).then(function(result) {
-              if (result.error) {
-                // Inform the customer that there was an error.
-                console.error(error);
-              }
-              console.log("Possibly successful payment")
-              console.log(result)
-            }).catch(console.error);
+    card(options) {
+      return this.requestPayment('card', options).then(({ data }) => {
+        this.stripe
+          .confirmCardPayment(data.response.client_secret, {
+            payment_method: { card: this.cardElem },
           })
-      },
-      multibanco: function(options) {
-        return this._requestPayment('multibanco', options)
-          .then(({data}) => data.response)
-          .then(({
-            multibanco, status, currency, amount
-          }) => ({
-            multibanco: {
-              entity: multibanco.entity,
-              reference: multibanco.reference
-            }, status, currency, amount
-          }))
-      },
-      pay: function({payment_method, ...options}) {
-        return this[payment_method](options)
-      },
-      ack: function(id, status) {
-        return this._axios.put(`/payment/instances/${id}/ack`, {status})
-      },
-      get: function(query) {
-        return this._axios.get(`/payment/instances`, query)
-      }
-    }
-  }
-  
-  module.exports = Payment
+          .then(result => {
+            if (result.error) {
+              // Inform the customer that there was an error.
+              console.error(error);
+            }
+            console.log('Possibly successful payment');
+            console.log(result);
+          })
+          .catch(console.error);
+      });
+    },
+
+    multibanco(options) {
+      return this.requestPayment('multibanco', options)
+        .then(({ data }) => data.response)
+        .then(({ multibanco, status, currency, amount }) => ({
+          multibanco: {
+            entity: multibanco.entity,
+            reference: multibanco.reference,
+          },
+          status,
+          currency,
+          amount,
+        }));
+    },
+
+    pay({ payment_method, ...options }) {
+      return this[payment_method](options);
+    },
+
+    ack(id, status) {
+      return this.axios.put(`/payment/instances/${id}/ack`, { status });
+    },
+
+    get(query) {
+      return this.axios.get('/payment/instances', query);
+    },
+  };
+};
+
+module.exports = Payment;
